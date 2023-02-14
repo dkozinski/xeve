72ae8736ebd638a80697a40c22f4493fea7a8580
Dawid Kozinski <d.kozinski@samsung.com>
2023-02-14 14:23:49 +0100

    Removed unnecessary exmple files

--- debian/base/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(xeve-base):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="xeve-base" command="/usr/bin/xeve-base"
--- debian/base/debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-libxeve-base 0.4.3 xeve-base (>> 0.4.3-0), xeve-base (<< 0.4.3-99)
